ed2f03f79f98920c215e0f8501816e01fda0a372
Olga Filiushkina <olga.filiushkina@gmail.com>
2021-03-17 16:45:30 -0400

    created layout and styles for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWalking, faReceipt, faQrcode } from '@fortawesome/free-solid-svg-icons';
+
+
 const Card = ({ data }) => {
   
 
@@ -13,20 +17,44 @@ const Card = ({ data }) => {
     }
   }
 
+  //TODO: REFACTOR KICKS INTO A COMPONENT IF THERES TIME
   return (
     <li className="card">
       <img className="card__logo" src={logo} alt=""></img>
       <p className="card__name">{name}</p>
+
       <ul className="card__kicks">
+
         <li>
+          <div className="card__kicks-icon-container">
+            <FontAwesomeIcon 
+              className="card__kicks-icon" 
+              icon={faWalking} 
+              aria-hidden="true"/>
+          </div>
           <p>{walk_in} Walk-in{checkPlural(walk_in)}</p>
         </li>
+
         <li>
+          <div className="card__kicks-icon-container">
+            <FontAwesomeIcon 
+              className="card__kicks-icon" 
+              icon={faReceipt} 
+              aria-hidden="true" />
+          </div>
           <p>{receipt} Receipt{checkPlural(receipt)}</p>
         </li>
+
         <li>
+          <div className="card__kicks-icon-container">
+            <FontAwesomeIcon 
+              className="card__kicks-icon" 
+              icon={faQrcode} 
+              aria-hidden="true" />
+          </div>
           <p>{scan} Scan{checkPlural(scan)}</p>
         </li>
+        
       </ul>
     </li>
   )
